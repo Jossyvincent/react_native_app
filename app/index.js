1521d30a4a417dc948e8e2733df333769f8488df
Vincent Jossy Bonyo <vincentjossy0@gmail.com>
2025-07-01 14:19:56 +0300

    the set up

---
+++ app/index.js
@@ -0,0 +1,24 @@
+import {useState} from "react";/* lets components track internal state */
+
+import { View, ScrollView, SafeAreaView } from "react-native";/* react native Ui components */
+import { router, Stack, useRouter } from "expo-router"; /* defines a stack-based navigation */
+
+import {COLORS, icons,images, SIZES} from '../constants';
+
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
+
+
+const Home = () => {
+
+  const router = useRouter();
+    return (
+      <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite }}>
+        <Stack.Screen
+        options={{headerStyle: {backgroundColor: COLORS.lightWhite }}}
+        />
+      </SafeAreaView>
+    )
+}
+
+export default Home;
+ 
